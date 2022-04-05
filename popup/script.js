741abffcb895a213eda5cf40e4050b60b5575c14
chewbaccapush <gasper.lukacs@gmail.com>
2022-04-05 14:07:00 +0200

    Frontend

---
+++ popup/script.js
@@ -0,0 +1,15 @@
+
+
+let haha = document.getElementById('encrypt');
+console.log(haha); 
+
+/*
+document.getElementById('encrypt').addEventListener("click", function(){
+    console.log("clicked");
+    
+    let winId = browser.windows.WINDOW_ID_CURRENT;
+    let removing = browser.windows.remove(winId);
+    let creating = browser.windows.create(createData);
+    
+});
+*/
