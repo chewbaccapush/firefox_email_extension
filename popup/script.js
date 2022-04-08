7499e94105f221622aaeb076842e5b92ceb163a1
chewbaccapush <gasper.lukacs@gmail.com>
2022-04-08 15:04:38 +0200

    frontend requesti

--- popup/script.js
+++ popup/script.js
@@ -1,14 +1,22 @@
 
-//Dinamično nalaganje ključev v tabelo in dropdown
+
 window.onload = function() {
     //Get keys from storage...
+    generateKeysTable();
+  
+  
+};
 
+//Dinamično nalaganje ključev v tabelo in dropdown
+const generateKeysTable = async() => {
+    // let userId = localStorage.getItem('userId') || 0;
+    // let keys = await getKeys(userId);
     let keys = [];
 
     let select = document.createElement("select");
     select.name = "keys";
     select.id = "keys";
- 
+
     for (const key of keys)
     {
         var option = document.createElement("option");
@@ -16,37 +24,93 @@ window.onload = function() {
         option.name = key.name;
         select.appendChild(option);
     }
- 
+
     let label = document.createElement("label");
     label.id = "labelInput"
     label.innerText = "Select a key: "
     label.htmlFor = "keys";
- 
-    document.getElementById("container").appendChild(label).appendChild(select);
 
-};
+    document.getElementById("container").appendChild(label).appendChild(select);
+}
+
+//Get keys of a user
+const getKeys = async(id) => {
+   
+    fetch("http://127.0.0.1:5000/getKeysById" + query,
+    {
+        method: "POST",
+        body: JSON.stringify(id)
+    })
+    .then((res) => {
+        console.log(res)
+        if (res.isJson()) return JSON.parse(res)
+        else return res;
+    })
+    .catch((e) => { console.log(e) })
+}
 
 //Encrypt button
-const encryptMessage = () => {
+const encryptMessage = async() => {
     let recipient = document.getElementById('recipient').value;
-    let messege = document.getElementById('message').value;
+    let message = document.getElementById('message').value;
     let key = document.getElementById('keys').value;
+
+    let toEncrypt = {recipient: recipient, message: message, key: key};
+    let encryptedFile;
+
+    fetch("http://127.0.0.1:5000/encrypt",
+    {
+        method: "POST",
+        body: JSON.stringify(toEncrypt)
+    })
+    .then((res) => {
+        console.log(res);
+        if (res.isJson()) encryptedFile = JSON.parse(res);
+        else encryptedFile = res;
+    })
+    .catch((e) => {console.log(e)})
 }
 
 //Decrypt button
 const decryptMessage = () => {
     let encryptedFile = document.getElementById('fileInput').value;
+    let decryptedFile;
+
+    fetch("http://127.0.0.1:5000/decrypt",
+    {
+        method: "POST",
+        body: JSON.stringify(encryptedFile)
+    })
+    .then((res) => {
+        console.log(res);
+        if (res.isJson()) decryptedFile = JSON.parse(res);
+        else decryptedFile = res;
+    })
+    .catch((e) => {console.log(e)})
+
 
 
 }
 
 //Generate key
-const generateKey = () => {
-    fetch('http://127.0.0.1:5000/generateKey')
-        .then(response => response.json())
-        .then(data => console.log(data));
-   
-    //Shrani ključ v storage
+const generateKey = async() => {
+    let email = document.getElementById('email').value;
+    let password = document.getElementById('password').value;
+
+    let user = {email: email, password: password};
+ 
+    fetch("http://127.0.0.1:5000/generateKey",
+    {
+        method: "POST",
+        body: JSON.stringify(user)
+    })
+    .then((res) => {
+        console.log(res);
+        localStorage.setItem('userId', res.userId);
+        generateKeysTable();
+    })
+    .catch((e) => {console.log(e)})
+
 }
 
 //Za dinamično dodajanje v tabelo
